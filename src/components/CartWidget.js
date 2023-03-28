26c6c671da8c5e841d22db3ee0b511376849a0e0
Franco Rodriguez Dodera <francododera@hotmail.com>
2023-03-28 20:37:42 -0300

    PreEntrega1+RodriguezDodera

---
+++ src/components/CartWidget.js
@@ -0,0 +1,7 @@
+function cartWidget() {
+    return(
+        <button id="carrito"><i className="fas fa-shopping-cart"></i><span>0</span></button>
+    )
+}
+
+export default cartWidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+function ItemListContainer() {
+  return (
+    <div>
+      <h1>No tenemos stock disponible!!!</h1>
+    </div>
+  );
+}
+
+export default ItemListContainer;
